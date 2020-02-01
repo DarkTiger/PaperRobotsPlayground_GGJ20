0b25f305a749dcee3bf22c5e2ee674aaaa50ce99
DarkTiger <darktigerdevelop@gmail.com>
2020-02-01 10:49:37 +0100

    Updated Player and added PlayerAttack script

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] int damage = 1;
+    int playerIndex;
+
+    private void Start()
+    {
+        playerIndex = GetComponent<PlayerStats>().playerIndex;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("FireP" + playerIndex))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, LayerMask.GetMask("Player")))
+            {
+                hit.collider.GetComponent<PlayerStats>().Damage(damage);
+            }
+        }
+    }
+}
