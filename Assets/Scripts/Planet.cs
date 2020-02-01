28011503e910a56ac7a8f43f836023efdde1cc32
DarkTiger <darktigerdevelop@gmail.com>
2020-02-01 01:53:12 +0100

    Updates planet with procedural objects

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject[] envObjects = null;
+    [SerializeField] int count = 100;
+
+    private void Start()
     {
-        
+        StartCoroutine(SpawnEnvObjects());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnEnvObjects()
     {
-        
+        float currentCount = 0;
+
+        while (currentCount < count)
+        {
+            GameObject envObject = envObjects[Random.Range(0, envObjects.Length)];
+            Vector3 position = transform.position + new Vector3(Random.Range(-400, 400), Random.Range(-400, 400), Random.Range(-400, 400));
+            Vector3 direction = (transform.position - position).normalized;
+            RaycastHit hit;
+
+            if (Physics.Raycast(position, direction, out hit, LayerMask.GetMask("Planet")))
+            {
+                Transform envObjectInstantiated = Instantiate(envObject, hit.point, Quaternion.identity).transform;
+                envObjectInstantiated.up = (envObjectInstantiated.position - transform.position).normalized;
+                currentCount++;
+            }
+            yield return null;
+        }
     }
 }
